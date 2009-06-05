30d02995c1cbd4efb1e8eb29caa79ada06b1ad0d
Henrik Bengtsson <hb@aroma-project.org>
2009-06-05 20:32:09 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@1529 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/999.NonDocumentedObjects.R
+++ R/999.NonDocumentedObjects.R
@@ -2,7 +2,7 @@
 # @RdocDocumentation "Non-documented objects"
 #
 # % Other missing docs
-# @eval "t <- readLines('999.missingdocs.txt'); t <- trim(unlist(strsplit(t, split=' '))); t <- t[nchar(t) > 0]; t2 <- gsub('\\[', '\\\\[', t); t <- unique(t); t <- sprintf('\\alias{%s}', t); paste(t, collapse='\n')"
+# @eval "t <- readLines('../incl/999.missingdocs.txt'); t <- trim(unlist(strsplit(t, split=' '))); t <- t[nchar(t) > 0]; t2 <- gsub('\\[', '\\\\[', t); t <- unique(t); t <- sprintf('\\alias{%s}', t); paste(t, collapse='\n')"
 #
 # \description{
 #   This page contains aliases for all "non-documented" objects that 

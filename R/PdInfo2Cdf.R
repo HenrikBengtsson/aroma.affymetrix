96b634e59769963d266a2709092ff6ed9d151fc0
Henrik Bengtsson <hb@aroma-project.org>
2009-10-17 05:48:55 +0000

    o CLEAN UP: Cleaned up dependencies in DESCRIPTION.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@1673 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/PdInfo2Cdf.R
+++ R/PdInfo2Cdf.R
@@ -1,62 +1,114 @@
-
-PdInfo2Cdf <- function( pdpkg, celfile, verbose=TRUE, overwrite=FALSE ) {
-
-  #### This script has been written to generate a .cdf-file from an "pd.XXXX" package, 
-  #### such as those build with pdInfoBuilder.
+PdInfo2Cdf <- function(pdpkg, celfile, verbose=TRUE, overwrite=FALSE) {
+  #### This script has been written to generate a .cdf-file from an 
+  #### "pd.XXXX" package, such as those build with pdInfoBuilder.
   #### The original was written by Samuel Wuest, modified by Mark Robinson 
   #### (around 12 Jan 2009) to be generic
 
-  require("affxparser") || stop("Package not loaded: affxparser");
-  require("pdInfoBuilder") || stop("Package not loaded: pdInfoBuilder");
+  require("affxparser") || throw("Package not loaded: affxparser");
+  require("pdInfoBuilder") || throw("Package not loaded: pdInfoBuilder");
 
-  do.call("library",args=list(package=pdpkg))
-
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Local functions
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   pmFeature2List <- function(u) {
-    nr <- nrow(u)
-    o <- order(u$atom)
-    v <- 0:(nr-1)
-    id <- u$fsetid[1]
-    g <- list(list(x=u$x[o], y=u$y[o], pbase=rep("T",nr),tbase=rep("A",nr),
-              atom=v,indexpos=v,groupdirection="sense",natoms=nr,ncellsperatom=1))
-    names(g) <- id
-    list(unittype=1,unitdirection=1,groups=g,natoms=nr,ncells=nr,ncellsperatom=1,unitnumber=id)
-  }
+    nr <- nrow(u);
+    o <- order(u$atom);
+    v <- 0:(nr-1);
+    id <- u$fsetid[1];
+    g <- list(list(x=u$x[o], y=u$y[o], pbase=rep("T", nr), tbase=rep("A", nr),
+              atom=v, indexpos=v, groupdirection="sense", natoms=nr,
+              ncellsperatom=1));
+    names(g) <- id;
+    list(unittype=1, unitdirection=1, groups=g, natoms=nr, ncells=nr,
+         ncellsperatom=1, unitnumber=id);
+  } # pmFeature2List()
 
-  if(verbose)
-    cat("Reading CEL file.\n")
-  cel <- read.celfiles(filenames=celfile,pkgname=pdpkg)
-  pd <-getPlatformDesign(cel)
-  if(verbose)
-    cat("Querying Platform Design database.\n")
-  ff <- dbGetQuery(db(pd), "select * from pmfeature")
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validating arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'celfile':
+  celfile <- Arguments$getReadablePathname(celfile, mustExist=TRUE);
 
-  celHead <- readCelHeader(celfile)
-  nrows <- celHead$rows
-  ncols <- celHead$rows
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+
+  # Argument 'overwrite':
+  overwrite <- Arguments$getLogical(overwrite);
 
 
+  verbose && enter(verbose, "PdInfo2Cdf()");
+
+  filename <- sprintf("%s.cdf", pdName);
+  filename <- Arguments$getWritablePathname(filename, mustNotExist=!overwrite);
+
+  # Load the required PD package.
+  require(pdpkg, character.only=TRUE) || 
+                 throw("Platform Design (PD) package not loaded: ", pdpkg);
+
+  verbose && cat(verbose, "Reading CEL file.");
+
+  verbose && enter(verbose, "Reading CEL file");
+  cel <- read.celfiles(filenames=celfile, pkgname=pdpkg);
+  verbose && exit(verbose);
+
+  pd <- getPlatformDesign(cel);
+  rm(cel);  # Not needed anymore
+
+  verbose && enter(verbose, "Querying Platform Design database");
+  ff <- dbGetQuery(db(pd), "select * from pmfeature");
+  rm(pd);  # Not needed anymore
+  verbose && exit(verbose);
+
+  celHead <- readCelHeader(celfile);
+  nrows <- celHead$rows;
+  ncols <- celHead$rows;
+  rm(celHead);  # Not needed anymore
+
+  verbose && enter(verbose, "Creating list from query table");
   # three 3 lines speed up the splitting ...
-  if(verbose)
-    cat("Creating list from query table.\n")
-  ffs <- split(ff, substr(ff$fsetid,1,4))
-  ffs <- unlist( lapply(ffs, FUN=function(u) split(u,u$fsetid)), recursive=FALSE)
-  names(ffs) <- substr(names(ffs), 6,nchar(names(ffs)))
+  ffs <- split(ff, substr(ff$fsetid, 1,4));
+  ffs <- lapply(ffs, FUN=function(u) split(u, u$fsetid));
+  ffs <- unlist(ffs, recursive=FALSE);
+  names(ffs) <- substr(names(ffs), 6, nchar(names(ffs)));
+  verbose && exit(verbose);
 
-  nunits <- length(ffs)
+  nunits <- length(ffs);
+  pdName <- gsub("\\.", "", pdpkg);
 
-  pdName <- gsub("\\.","",pdpkg)
+  ## creating the CDF header;
+  newCdfHeader <- list(ncols=ncols, nrows=nrows, nunits=nunits, 
+                       nqcunits=0, refseq="", chiptype=pdName, 
+                       filename=filename, rows=nrows, 
+                       cols=ncols, probesets= nunits, 
+                       qcprobesets=0, reference="");
 
-  ## creating the cdf-header;
-  newCdfHeader <- list(ncols=ncols, nrows=nrows, nunits=nunits, nqcunits=0, refseq="", 
-                       chiptype=pdName, filename=paste(pdName,"cdf",sep="."), rows=nrows, 
-                       cols=ncols, probesets= nunits, qcprobesets= 0, reference="")
+  verbose && enter(verbose, "Creating CDF list structure");
+  ### make the input-list for writeCdf()
+  verbose && cat(verbose, "Creating CDF list for ", nunits, " units");
+  newCdfList <- lapply(ffs, FUN=pmFeature2List);
+  rm(ffs);  # Not needed anymore
+  verbose && exit(verbose);
 
-  ### make the input-list for the writeCdf-function
-  if(verbose)
-    cat(paste("Creating CDF list for", nunits, "units.\n"))
-  newCdfList <- lapply(ffs, pmFeature2List)
+  verbose && enter(verbose, "Writing CDF file");
+  ### writing the CDF file (binary-file)
+  res <- writeCdf(newCdfHeader$filename, cdfheader=newCdfHeader, 
+          cdf=newCdfList, cdfqc=NULL, verbose=verbose, overwrite=overwrite);
+  verbose && exit(verbose);
 
-  ### writing the cdf-file (binary-file)
-  writeCdf(newCdfHeader$filename, cdfheader= newCdfHeader, cdf=newCdfList, cdfqc=NULL, verbose=verbose, overwrite=overwrite)
+  verbose && exit(verbose);
 
-}
+  invisible(res);
+} # PdInfo2Cdf()
+
+############################################################################
+# HISTORY:
+# 2009-10-16 [HB]
+# o MEMORY OPTIMIZATION: Cleaning out non needed objects sooner.
+# o Added verbose a'la R.utils.
+# o Added some validation of arguments.
+# o Tidied up the code structure.
+# 2009-01-13 [MR]
+# o Added.
+# 2008-??-?? [SW]
+# o Created by Samuel Wuest.
+############################################################################

8db13abc7032a9de3878137d8bed76a95c3a96b2
Henrik Bengtsson <hb@aroma-project.org>
2009-11-19 17:03:54 +0000

    One test script is no longer compatible with aroma.core v1.3.3.  This is known and expected.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@1744 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/testScripts/system/chipTypes/Mapping10K_Xba142/test20070726,10K,QA.R
+++ inst/testScripts/system/chipTypes/Mapping10K_Xba142/test20070726,10K,QA.R
@@ -88,36 +88,41 @@ nuse <- extractDataFrame(ces, units=units, field="NUSE", addNames=TRUE);
 # Robust estimate of standard deviation of raw CNs
 # (The default is to use a first-order difference variance estimator)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Estimate it for some autosomal chromosomes and ChrX
-cns <- CbsModel(ces);
-res <- estimateSds(cns, chromosomes=c(1:6, 23), verbose=log);
-chrX <- which(rownames(res) == "23");
-
-layout(matrix(1:2, nrow=2));
-par(mar=c(4,4,0.5,2)+0.1);
-
-xlim <- c(1,(ncol(res)+2));
-ylim <- c(0, 1.05*max(res, na.rm=TRUE));
-ylab <- expression(hat(sigma)==s[Delta](log2(theta/theta[R])));
-cols <- seq(length=nrow(res));
-ltys <- rep(1, times=nrow(res));
-cols[chrX] <- "blue";
-ltys[chrX] <- 4;
-
-plot(NA, xlim=xlim, ylim=ylim, xlab="", ylab=ylab);
-for (kk in seq(length=nrow(res))) {
-  chromosome <- rownames(res)[kk];
-  points(res[kk,], col=cols[kk], pch=19);
-  lines(res[kk,], col=cols[kk], lty=ltys[kk], lwd=2);
-}
-legend("topright", col=cols, lty=ltys, pch=19, lwd=2, 
-                   legend=sprintf("Chr%s", rownames(res)), bty="n");
-
-
-df <- as.data.frame(res[-chrX,,drop=FALSE]);
-colnames(df) <- seq(length=nrow(df));
-boxplot(df, ylim=ylim, ylab=ylab, xlab="Array");
-points(res[chrX,], col="blue", pch=19, cex=1.5);
-legend("bottomright", col=c("black", cols[chrX]), pch=19, lwd=2, 
-                   legend=c("Autosomal", "ChrX"), horiz=TRUE, bty="n");
-devDone();
+if (FALSE) {
+  # Estimate it for some autosomal chromosomes and ChrX
+  # From aroma.core v1.3.3 it is no longer possible to define
+  # a segmentation model if the data set is not a copy number
+  # data set. /HB 2009-11-19
+  cns <- CbsModel(ces);
+  res <- estimateSds(cns, chromosomes=c(1:6, 23), verbose=log);
+  chrX <- which(rownames(res) == "23");
+  
+  layout(matrix(1:2, nrow=2));
+  par(mar=c(4,4,0.5,2)+0.1);
+  
+  xlim <- c(1,(ncol(res)+2));
+  ylim <- c(0, 1.05*max(res, na.rm=TRUE));
+  ylab <- expression(hat(sigma)==s[Delta](log2(theta/theta[R])));
+  cols <- seq(length=nrow(res));
+  ltys <- rep(1, times=nrow(res));
+  cols[chrX] <- "blue";
+  ltys[chrX] <- 4;
+  
+  plot(NA, xlim=xlim, ylim=ylim, xlab="", ylab=ylab);
+  for (kk in seq(length=nrow(res))) {
+    chromosome <- rownames(res)[kk];
+    points(res[kk,], col=cols[kk], pch=19);
+    lines(res[kk,], col=cols[kk], lty=ltys[kk], lwd=2);
+  }
+  legend("topright", col=cols, lty=ltys, pch=19, lwd=2, 
+                     legend=sprintf("Chr%s", rownames(res)), bty="n");
+  
+  
+  df <- as.data.frame(res[-chrX,,drop=FALSE]);
+  colnames(df) <- seq(length=nrow(df));
+  boxplot(df, ylim=ylim, ylab=ylab, xlab="Array");
+  points(res[chrX,], col="blue", pch=19, cex=1.5);
+  legend("bottomright", col=c("black", cols[chrX]), pch=19, lwd=2, 
+                     legend=c("Autosomal", "ChrX"), horiz=TRUE, bty="n");
+  devDone();
+} # if (FALSE)

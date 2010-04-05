319d3dfdbace2db3860c237fd1460e01dc0c29b0
Henrik Bengtsson <hb@aroma-project.org>
2010-04-05 12:33:25 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@1946 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/doCRMAv1.R
@@ -0,0 +1,139 @@
+setMethodS3("doCRMAv1", "AffymetrixCelSet", function(csR, combineAlleles=TRUE, arrays=NULL, ..., ram=NULL, verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Argument 'csR':
+  className <- "AffymetrixCelSet";
+  if (!inherits(csR, className)) {
+    throw(sprintf("Argument 'csR' is not a %s: %s", className, class(csR)[1]));
+  }
+
+  # Argument 'combineAlleles':
+  combineAlleles <- Arguments$getLogical(combineAlleles);
+
+  # Argument 'arrays':
+  if (!is.null(arrays)) {
+    throw("Not supported. Argument 'arrays' should be NULL.");
+    arrays <- Arguments$getIndices(arrays, max=nbrOfArrays(csR));
+  }
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+
+
+  verbose && enter(verbose, "CRMAv1");
+  verbose && cat(verbose, "Arguments:");
+  verbose && cat(verbose, "combineAlleles: ", combineAlleles);
+  arraysTag <- seqToHumanReadable(arrays);
+  verbose && cat(verbose, "arrays:");
+  verbose && str(verbose, arraysTag);
+  verbose && cat(verbose, "ram: ", ram);
+
+
+  verbose && cat(verbose, "Data set");
+  verbose && print(verbose, csR);
+
+  if (!is.null(arrays)) {
+    verbose && enter(verbose, "CRMAv1/Extracting subset of arrays");
+    csR <- extract(csR, arrays);
+    verbose && cat(verbose, "Data subset");
+    verbose && print(verbose, csR);
+    verbose && exit(verbose);
+  }
+
+  verbose && enter(verbose, "CRMAv1/Allelic crosstalk calibration");
+  acc <- AllelicCrosstalkCalibration(csR, model="CRMAv1");
+  verbose && print(verbose, acc);
+  csC <- process(acc, verbose=verbose);
+  verbose && print(verbose, csC);
+  verbose && exit(verbose);
+
+  # Clean up
+  rm(csR, acc);
+  gc <- gc();
+  verbose && print(verbose, gc);
+
+  verbose && enter(verbose, "CRMAv1/Probe summarization");
+  plm <- RmaCnPlm(csN, mergeStrands=TRUE, combineAlleles=combineAlleles);
+  verbose && print(verbose, plm);
+  if (length(findUnitsTodo(plm)) > 0) {
+    # Fit CN probes quickly (~5-10s/array + some overhead)
+    units <- fitCnProbes(plm, verbose=verbose);
+    verbose && str(verbose, units);
+    # Fit remaining units, i.e. SNPs (~5-10min/array)
+    units <- fit(plm, ram=ram, verbose=verbose);
+    verbose && str(verbose, units);
+    rm(units);
+  }  
+  verbose && print(verbose, gc);
+  ces <- getChipEffectSet(plm);
+  verbose && print(verbose, ces);
+  verbose && exit(verbose);
+
+  # Clean up
+  rm(plm, csN);
+  gc <- gc();
+  
+  verbose && enter(verbose, "CRMAv1/PCR fragment-length normalization");
+  fln <- FragmentLengthNormalization(ces, target="zero");
+  verbose && print(verbose, fln);
+  cesN <- process(fln, verbose=verbose);
+  verbose && print(verbose, cesN);
+  verbose && exit(verbose);
+
+  # Clean up
+  rm(fln, ces);
+  gc <- gc();
+  
+  verbose && enter(verbose, "CRMAv1/Export to technology-independent data files");
+  dsNList <- exportTotalAndFracB(cesN, verbose=verbose);
+  verbose && print(verbose, dsNList);
+  verbose && exit(verbose);
+
+  # Clean up
+  rm(cesN);
+  gc <- gc();
+
+  verbose && exit(verbose);
+
+  dsNList;
+}) # doCRMAv1()
+
+
+setMethodS3("doCRMAv1", "character", function(dataSet, ..., verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'dataSet':
+  dataSet <- Arguments$getCharacter(dataSet);
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+
+
+  verbose && enter(verbose, "CRMAv1");
+
+  verbose && enter(verbose, "CRMAv1/Setting up CEL set");
+  csR <- AffymetrixCelSet$byName(dataSet, ..., verbose=less(verbose, 50),
+                                                  .onUnknownArgs="ignore");
+  verbose && print(verbose, csR);
+  verbose && exit(verbose);
+
+  dsNList <- doCRMAv1(csR, ..., verbose=verbose);
+
+  # Clean up
+  rm(csR);
+  gc <- gc();
+
+  verbose && exit(verbose);
+
+  dsNList;
+})
+
+
+############################################################################
+# HISTORY:
+# 2010-04-04
+# o Created from doCRMAv2.R.
+# o (Re)created.
+############################################################################

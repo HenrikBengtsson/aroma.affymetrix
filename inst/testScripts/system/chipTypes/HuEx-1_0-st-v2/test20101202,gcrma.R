42b3c6ddf07271d91090019a7725fa8dff6e7270
Henrik Bengtsson <hb@aroma-project.org>
2010-12-04 02:19:30 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2097 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/testScripts/system/chipTypes/HuEx-1_0-st-v2/test20101202,gcrma.R
+++ inst/testScripts/system/chipTypes/HuEx-1_0-st-v2/test20101202,gcrma.R
@@ -9,6 +9,7 @@ name <- "Affymetrix-HeartBrain";
 chipType <- "HuEx-1_0-st-v2";
 
 cdf <- AffymetrixCdfFile$byChipType(chipType, tags="coreR3,A20071112,EP");
+cdf <- AffymetrixCdfFile$byChipType(chipType, tags="fullR3,A20071112,EP");
 print(cdf);
 
 # Setup CEL set using the core CDF.
@@ -19,7 +20,10 @@ print(csR);
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Background correction and normalization
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cdfS <- AffymetrixCdfFile$byChipType(getChipType(cdf, fullname=FALSE));
+setCdf(csR, cdfS);
 bc <- GcRmaBackgroundCorrection(csR, type="affinities");
 print(bc);
 csB <- process(bc, verbose=verbose);
 print(csB);
+setCdf(csB, cdf);

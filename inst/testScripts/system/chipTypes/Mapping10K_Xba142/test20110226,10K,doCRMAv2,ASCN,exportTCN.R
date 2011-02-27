db3641af8bbd9682d589b3164d2b9379bb8d892b
Henrik Bengtsson <hb@aroma-project.org>
2011-02-27 01:32:01 +0000

    Added a test script for non-public exportTotalCnRatioSet().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2146 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/testScripts/system/chipTypes/Mapping10K_Xba142/test20110226,10K,doCRMAv2,ASCN,exportTCN.R
@@ -0,0 +1,43 @@
+##########################################################################
+# Data set:
+# GSE8605/
+#   Mapping10K_Xba142/
+#    GSM226867.CEL, ..., GSM226876.CEL [10 files]
+# URL: http://www.ncbi.nlm.nih.gov/projects/geo/query/acc.cgi?acc=GSE8605
+##########################################################################
+library("aroma.affymetrix");
+verbose <- Arguments$getVerbose(-8, timestamp=TRUE);
+
+dataSet <- "GSE8605";
+chipType <- "Mapping10K_Xba142";
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# doASCRMAv2()
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+dsList <- doASCRMAv2(dataSet, chipType=chipType, verbose=verbose);
+print(dsList);
+
+
+dsT <- extract(dsList$total, 1:5);
+dsR <- extract(dsList$total, 6:10);
+
+dsTCN <- exportTotalCnRatioSet(dsT, dsR, logBase=NULL, verbose=verbose);
+
+dfAvg <- getAverageFile(dsTCN, verbose=-10);
+dfAvgLog <- getAverageFile(dsTCN, g=log2, h=function(x) 2^x, verbose=-10);
+
+diff <- dfAvg[,1] - dfAvgLog[,1];
+stopifnot(all(diff == 0));
+
+# Change the fullname (using a fullname translator)
+setFullName(dfAvg, "TumorVsNormal");
+
+# Turn into a data set
+dsAvg <- newInstance(dsT, list(dfAvg));
+
+# Segmentation model
+cbs <- CbsModel(dsAvg, tags="*,TCN,avg");
+cbs$.calculateRatios <- FALSE;
+
+ce <- ChromosomeExplorer(cbs);
+process(ce, chromosomes=c(1:5,19,22), verbose=verbose);

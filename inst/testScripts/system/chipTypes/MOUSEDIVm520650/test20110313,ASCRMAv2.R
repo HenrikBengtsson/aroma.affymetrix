7292cb39420f8c9e1838f46dc84602cf8cd5fce8
Henrik Bengtsson <hb@aroma-project.org>
2011-03-14 04:05:30 +0000

    Added a redundancy test for AS-CRMAv2 on MOUSEDIVm520650.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2188 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/testScripts/system/chipTypes/MOUSEDIVm520650/test20110313,ASCRMAv2.R
@@ -0,0 +1,28 @@
+library("aroma.affymetrix")
+
+verbose <- Arguments$getVerbose(-10, timestamp=TRUE);
+
+dataSetName <- "Affymetrix-MouseDiversity,Set1of5";
+chipType <- "MOUSEDIVm520650";
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setting up CEL set and locating the CDF file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cdf <- AffymetrixCdfFile$byChipType(chipType);
+print(cdf);
+
+ugp <- getAromaUgpFile(cdf);
+print(ugp);
+
+ufl <- getAromaUflFile(cdf);
+print(ufl);
+
+csR <- AffymetrixCelSet$byName(dataSetName, cdf=cdf);
+print(csR);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# AS-CRMAv2
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+dsList <- doASCRMAv2(csR, verbose=verbose);
+print(dsList);

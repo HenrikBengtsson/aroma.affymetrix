c3100ac0cee74898d94514f8fdc1427c48908491
Henrik Bengtsson <hb@aroma-project.org>
2011-08-02 01:40:54 +0000

    Version: 2.1.7 [2011-08-01] o WORKAROUND: Added similar workarounds as in v2.1.6 for more methods   defined in aroma.core, apply(), rowSums() etc.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2252 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,27 @@
 # Allows conflicts. For more information, see library() and
 # conflicts() in [R] base.
-.conflicts.OK <- TRUE
+.conflicts.OK <- TRUE;
 
 # WORKAROUND: In order for the package to work with the most recent
 # version of R devel, which automatically add namespaces to packages
 # who do not have one, we explicitly have specify the following.
 # /HB 2011-07-27
+# R.utils:
 cat <- R.utils::cat; 
 getOption <- R.utils::getOption;
-
-append <- R.filesets::append;
 lapply <- R.utils::lapply;
+
+# R.filesets:
+append <- R.filesets::append;
 sapply <- R.filesets::sapply;
+
+# aroma.core:
+apply <- aroma.core::apply;
+colMeans <- aroma.core::colMeans;
+colSums <- aroma.core::colSums;
+library <- aroma.core::library;
+require <- aroma.core::require;
+.Machine <- aroma.core::.Machine;
 
 
 .First.lib <- function(libname, pkgname) {

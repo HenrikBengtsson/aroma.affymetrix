3f36babf1f7d816b12a1a645d58287f8af1ddaf2
Henrik Bengtsson <hb@aroma-project.org>
2012-08-29 00:19:48 +0000

    Version: 2.5.7 [2012-08-28] o SPEED UP: Now doCRMAv2() returns even faster if already processed. o ROBUSTNESS: Added a systems test for running doASCRMAv2() in   parallel using parLapply().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2481 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/testScripts/parallel/chipTypes/Mapping10K_Xba142/test20120605,10K,doCRMAv2,ASCN.R
@@ -0,0 +1,34 @@
+##########################################################################
+# Data set:
+# GSE8605/
+#   Mapping10K_Xba142/
+#    GSM226867.CEL, ..., GSM226876.CEL [10 files]
+# URL: http://www.ncbi.nlm.nih.gov/projects/geo/query/acc.cgi?acc=GSE8605
+##########################################################################
+library("aroma.affymetrix");
+verbose <- Arguments$getVerbose(-8, timestamp=TRUE);
+
+dataSet <- "GSE8605";
+chipType <- "Mapping10K_Xba142";
+
+dsR <- AffymetrixCelSet$byName(dataSet, chipType=chipType);
+print(dsR);
+
+library("parallel");
+cl <- makeCluster(2L);
+
+clusterExport(cl, "dsR");
+clusterExport(cl, "verbose");
+
+res <- parLapply(cl, X=seq(dsR), fun=function(ii) {
+  library("aroma.affymetrix");
+  verbose && enter(verbose, sprintf("Array #%d", ii));
+  dsListII <- doASCRMAv2(dsR, arrays=ii);
+  verbose && exit(verbose);
+  dsListII;
+});
+
+# Load result
+dsNList <- doASCRMAv2(dsR);
+print(dsNList);
+

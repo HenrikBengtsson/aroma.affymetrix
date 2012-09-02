b4808ecc2d32d866a2fa1d3fb930961b503d8182
Henrik Bengtsson <hb@aroma-project.org>
2012-09-02 01:25:17 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2503 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/testScripts/replication/chipTypes/Mapping50K_Hind240,Xba240/01b.downloadRawData.R
@@ -0,0 +1,42 @@
+path <- system.file("testScripts", package="aroma.affymetrix");
+pathname <- file.path(path, "downloadUtils.R");
+source(pathname);
+
+verbose && enter(verbose, "Downloading raw data");
+
+
+##########################################################################
+# Data set:
+# HapMap,CEU/
+#   Mapping50K_Hind240/
+#    GSM226867.CEL, ..., GSM226876.CEL [10 files]
+#
+# Overall design:
+#  [Ten] cervival cancer cell lines were hybridized to Affymetrix 
+#  Focus arrays in duplicate. Correlations were made with copynumber 
+#  profiles from arrayCGH and SNP arrays.
+#
+# URL: http://www.ncbi.nlm.nih.gov/projects/geo/query/acc.cgi?acc=GSE8605
+##########################################################################
+dataSet <- "HapMap,CEU";
+chipTypes <- c("Mapping50K_Hind240", "Mapping50K_Xba240");
+sampleNames <- c("CEU_NA06985", "CEU_NA06991", "CEU_NA06993",
+                 "CEU_NA06994", "CEU_NA07000", "CEU_NA07019");
+
+verbose && cat(verbose, "Data set: ", dataSet);
+
+ds <- downloadHapMapSamples(dataSet, chipType=chipTypes[1], sampleNames=sampleNames);
+print(ds);
+## AffymetrixCelSet:
+## Name: HapMap
+## Tags: CEU
+## Path: rawData/HapMap,CEU/Mapping50K_Hind240
+## Platform: Affymetrix
+## Chip type: Mapping50K_Hind240
+## Number of arrays: 6
+## Names: CEU_NA06985_HIND, CEU_NA06991_HIND, CEU_NA06993_HIND, ..., CEU_NA07019_HIND [6]
+## Time period: 2004-01-14 14:02:08 -- 2004-01-17 13:53:58
+## Total file size: 146.86MB
+## RAM: 0.01MB
+
+verbose && exit(verbose);
--- inst/testScripts/replication/chipTypes/Mapping50K_Hind240,Xba240/12.CRLMM_vs_oligo.R
+++ inst/testScripts/replication/chipTypes/Mapping50K_Hind240,Xba240/12.CRLMM_vs_oligo.R
@@ -9,71 +9,68 @@
 #
 # Author: Henrik Bengtsson
 # Created: 2008-12-04
-# Last modified: 2009-01-12
-#
-# Data set:
-#  rawData/
-#   HapMap270,100K,CEU,testSet/
-#     Mapping50K_Hind240/
-#       NA06985,Hind,B5,3005533.CEL
-#       NA06991,Hind,B6,3005533.CEL
-#       NA06993,Hind,B4,4000092.CEL
-#       NA06994,Hind,A7,3005533.CEL
-#       NA07000,Hind,A8,3005533.CEL
-#       NA07019,Hind,A12,4000092.CEL
+# Last modified: 2012-09-01
 ###########################################################################
 library("aroma.affymetrix");
-log <- Arguments$getVerbose(-8, timestamp=TRUE);
+library("oligo");
+verbose <- Arguments$getVerbose(-8, timestamp=TRUE);
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Setup data set
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-dataSet <- "HapMap270,100K,CEU,testSet";
+dataSet <- "HapMap,CEU";
 chipType <- "Mapping50K_Hind240";
 
 cdf <- AffymetrixCdfFile$byChipType(chipType);
 csR <- AffymetrixCelSet$byName(dataSet, cdf=cdf);
 print(csR);
 
+# Assert that the correct Platform Design package is installed
+pdPkgName <- cleanPlatformName(chipType);
+library(pdPkgName, character.only=TRUE);
+
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # CRLMM according to aroma.affymetrix
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-ces <- justSNPRMA(csR, normalizeToHapmap=TRUE, returnESet=FALSE, verbose=log);
+ces <- justSNPRMA(csR, normalizeToHapmap=TRUE, returnESet=FALSE, verbose=verbose);
 print(ces);
 
 recalibrate <- TRUE;
 crlmm <- CrlmmModel(ces, tags="*,oligo", recalibrate=recalibrate);
 print(crlmm);
 
-units <- fit(crlmm, ram="oligo", verbose=log);
+units <- fit(crlmm, ram="oligo", verbose=verbose);
 str(units);
 
 callSet <- getCallSet(crlmm);
 print(callSet);
 
+confSet <- getConfidenceScoreSet(crlmm);
+print(confSet);
+
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # CRLMM according to oligo
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-path <- file.path("oligoData", getFullName(csR), 
-                               getChipType(csR, fullname=FALSE));
-path <- Arguments$getWritablePathname(path);
+# Create temporary output directory for oligo
+path <- file.path("oligoData", dataSet, chipType);
+Arguments$getWritablePath(dirname(path)); # Create parent directories
 if (!isDirectory(path)) {
-  oligo:::justCRLMMv2(getPathnames(csR), tmpdir=path, recalibrate=recalibrate, balance=1.5, verbose=TRUE);
+  oligo:::justCRLMMv2(getPathnames(csR), tmpdir=path, recalibrate=recalibrate, balance=1.5, verbose=as.logical(verbose));
 }
+# Files created by oligo
+print(list.files(path));
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Compare genotype calls
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-units <- indexOf(cdf, pattern="^SNP");
-unitNames <- getUnitNames(cdf, units=units);
-units <- units[order(unitNames)];
-
-calls <- extractGenotypes(callSet, units=units, encoding="oligo");
-dimnames(calls) <- NULL;
-
 calls0 <- readSummaries("calls", path);
-dimnames(calls0) <- NULL;
+
+unitNames <- rownames(calls0);
+units <- indexOf(cdf, names=unitNames);
+calls <- extractGenotypes(callSet, units=units, encoding="oligo");
 
 count <- 0;
 for (cc in 1:ncol(calls)) {
@@ -95,26 +92,30 @@ stopifnot(errorRate < 5e-4);
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Compare confidence scores
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-confSet <- getConfidenceScoreSet(crlmm);
 conf <- extractMatrix(confSet, units=units);
-dimnames(conf) <- NULL;
-
 conf0 <- readSummaries("conf", path);
-dimnames(conf) <- NULL;
 
-delta <- conf - conf0;
-avgDelta <- mean(abs(delta), na.rm=TRUE);
-stopifnot(avgDelta < 1e-4);
+res <- all.equal(conf, conf0, check.attributes=FALSE);
+print(res);
 
-subplots(ncol(conf));
-par(mar=c(3,2,1,1)+0.1);
-lim <- c(0,1);
-for (cc in 1:ncol(conf)) {
-  plot(NA, xlim=lim, ylim=lim);
-  abline(a=0, b=1, col="#999999");
-  points(conf[,cc], conf0[,cc], pch=".", cex=3);
-  rho <- cor(conf[,cc], conf0[,cc]);
-  stext(side=3, pos=0, line=-1, sprintf("rho=%.4f", rho));
-  stext(side=3, pos=0, getNames(confSet)[cc]);
-  cat(sprintf("Array #%d: Correlation: %.4f\n", cc, rho));
-}
+# Sanity check
+stopifnot(all.equal(conf, conf0, check.attributes=FALSE, tolerance=1e-4));
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Visual comparison of confidence scores
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+toPNG(getFullName(ces), tags="CRLMM_vs_oligo", aspectRatio=0.8, {
+  subplots(ncol(conf));
+  par(mar=c(3,2,1,1)+0.1);
+  lim <- c(0,1);
+  for (cc in 1:ncol(conf)) {
+    plot(NA, xlim=lim, ylim=lim);
+    abline(a=0, b=1, col="#999999");
+    points(conf[,cc], conf0[,cc], pch=".", cex=3);
+    rho <- cor(conf[,cc], conf0[,cc]);
+    stext(side=3, pos=0, line=-1, sprintf("rho=%.4f", rho));
+    stext(side=3, pos=0, getNames(confSet)[cc]);
+    cat(sprintf("Array #%d: Correlation: %.4f\n", cc, rho));
+  }
+});

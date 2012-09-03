098951af5f52a843051e7abd278a000c7a530421
Henrik Bengtsson <hb@aroma-project.org>
2012-09-03 09:50:55 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2534 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/testScripts/system/chipTypes/HuEx-1_0-st-v2/test20101202,gcrma.R
+++ inst/testScripts/system/chipTypes/HuEx-1_0-st-v2/test20101202,gcrma.R
@@ -7,13 +7,20 @@ verbose <- Arguments$getVerbose(-3, timestamp=TRUE);
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 dataSet <- "Affymetrix-HeartBrain";
 chipType <- "HuEx-1_0-st-v2";
-
 cdf <- AffymetrixCdfFile$byChipType(chipType, tags="coreR3,A20071112,EP");
-cdf <- AffymetrixCdfFile$byChipType(chipType, tags="fullR3,A20071112,EP");
 print(cdf);
 
 # Setup CEL set using the core CDF.
 csR <- AffymetrixCelSet$byName(dataSet, cdf=cdf);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Process only cerebellum and heart
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+types <- c("cerebellum", "heart");
+csR <- extract(csR, indexOf(csR, patterns=types));
+
+setFullName(csR, sprintf("%s,%s", dataSet, paste(types, collapse="+")));
 print(csR);
 
 
@@ -24,6 +31,7 @@ cdfS <- AffymetrixCdfFile$byChipType(getChipType(cdf, fullname=FALSE));
 setCdf(csR, cdfS);
 bc <- GcRmaBackgroundCorrection(csR, type="affinities");
 print(bc);
+
 csB <- process(bc, verbose=verbose);
 print(csB);
 setCdf(csB, cdf);

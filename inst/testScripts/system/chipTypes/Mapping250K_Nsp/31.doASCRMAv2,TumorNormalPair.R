dfaade2daeb242cac7da7f6def5ef0567eebbda8
Henrik Bengtsson <hb@aroma-project.org>
2012-09-03 01:51:27 +0000

    Added system test scripts for just Mapping250K_Nsp
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2523 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/testScripts/system/chipTypes/Mapping250K_Nsp/31.doASCRMAv2,TumorNormalPair.R
@@ -0,0 +1,35 @@
+##########################################################################
+# Allele-specific CRMAv2 and Paired PSCBS
+#
+# Author: Henrik Bengtsson
+# Created on: 2011-11-11
+# Last updated: 2012-09-02
+##########################################################################
+library("aroma.affymetrix");
+verbose <- Arguments$getVerbose(-8, timestamp=TRUE);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+dataSet <- "GSE12702";
+chipType <- "Mapping250K_Nsp";
+
+csR <- AffymetrixCelSet$byName(dataSet, chipType=chipType);
+print(csR);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Extract a single tumor-normal pair
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Extract tumor-normal pair
+pair <- c(T="GSM318736", N="GSM318737");
+csR <- extract(csR, indexOf(csR, pair));
+print(csR);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# AS-CRMAv2
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+dsNList <- doASCRMAv2(csR, verbose=verbose);
+print(dsNList);

f12c5f4b204e0c89aa43bae049186f6d66c95e6e
Henrik Bengtsson <hb@aroma-project.org>
2012-09-04 05:01:11 +0000

    Now the Test3 scripts are also self contained.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2543 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/testScripts/system/chipTypes/Test3/01c.installPackages.R
@@ -0,0 +1,18 @@
+path <- system.file("testScripts", package="aroma.affymetrix");
+pathname <- file.path(path, "installUtils.R");
+source(pathname);
+
+library("R.utils");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+verbose && enter(verbose, "Installing test-specific packages");
+
+pkgs <- c("BioC:AffymetrixDataTestFiles");
+for (pkg in pkgs) {
+  verbose && cat(verbose, "Package: ", pkg);
+  installPkg(pkg);
+}
+
+verbose && exit(verbose);

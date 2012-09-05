e9562c8b10d78be0bc95e478033762b183331d09
Henrik Bengtsson <hb@aroma-project.org>
2012-09-05 19:49:02 +0000

    Added an (interactive) cleanup script.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2554 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/testScripts/setup/clean.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+rm -rf probeData/
+rm -rf plmData/
+rm -rf qcData/
+rm -rf firmaData/
+rm -rf cbsData/ gladData/ haarData/
+rm -rf totalAndFracBData/
+rm -rf totalAndFracBData,txt/
+rm -rf oligoData/ crlmmData/
+rm -rf Data/
+rm -rf rawCnData/ smoothCnData/
+rm -rf reports/
+
+rm -rf figures/
+rm Rplots.pdf
+rm Prec1_*.sgr
+rm replication*.png
+
+# WARNING
+## rm -rf annotationData/
+## rm -rf rawData/

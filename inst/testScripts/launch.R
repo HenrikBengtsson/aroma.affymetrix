a7b90a94bca66de1b49076b9172bce45fcea15be
Henrik Bengtsson <hb@aroma-project.org>
2012-10-20 03:00:22 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@2765 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/testScripts/launch.R
+++ inst/testScripts/launch.R
@@ -14,18 +14,33 @@
 # Rscript testScripts/launch.R --group=complete --pattern=GSE12702
 ############################################################################
 library("R.utils");
-path <- system.file("testScripts/R", package="aroma.affymetrix");
-pathname <- file.path(path, "launchUtils.R");
-source(pathname);
 
 # Override default settings with command line arguments  
 args <- commandArgs(asValues=TRUE, excludeReserved=TRUE, excludeEnvVars=TRUE);
 print(args);
 
+printf("Hostname: %s\n", System$getHostname());
+
+library("aroma.affymetrix");
+
+path <- system.file(package="aroma.affymetrix");
+path <- Arguments$getReadablePath(path);
+
+path <- file.path(path, "testScripts/R");
+path <- Arguments$getReadablePath(path);
+
+pathname <- file.path(path, "launchUtils.R");
+pathname <- Arguments$getReadablePathname(pathname);
+
+source(pathname);
+
 do.call(launchTestGroups, args);
 
 ############################################################################
 # HISTORY:
+# 2012-10-19
+# o ROBUSTNESS: Better pathname validation.
+# o Adding system details to output at the beginning.
 # 2012-09-14
 # o Created.
 ############################################################################

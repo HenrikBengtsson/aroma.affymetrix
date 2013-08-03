0b667bb529680ce856dbb6ecbedae859f63a9c2b
Henrik Bengtsson <hb@aroma-project.org>
2013-08-03 22:47:24 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.affymetrix@3133 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/009.setup.R
+++ R/009.setup.R
@@ -8,10 +8,10 @@
   # Bioconductor packages aroma.light and affxparser
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # require("aroma.light") - install if missing
-  .requireBioCPackage("aroma.light", neededBy=getName(pkg));
+  aroma.core:::.requireBiocPackage("aroma.light", neededBy=getName(pkg));
 
   # require("affxparser") - install if missing
-  .requireBioCPackage("affxparser", neededBy=getName(pkg));
+  aroma.core:::.requireBiocPackage("affxparser", neededBy=getName(pkg));
 
   # Make sure 'affxparser' is after 'aroma.affymetrix' on the search path
   from <- "package:affxparser";
@@ -57,40 +57,6 @@
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Bioconductor related
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- .requireBioCPackage <- function(package, neededBy="aroma.affymetrix", ...) {
-  # Trick 'R CMD check' to not generate NOTEs.
-  requireX <- base::require;
-  catX <- base::cat;
-
-  res <- suppressWarnings({
-    requireX(package, character.only=TRUE);
-  });
-
-  # Not installed?
-  if (!res) {
-    if (interactive()) {
-      # Trick 'R CMD check' to not generate NOTEs.
-      catX("Package '", package, "' is not available or could not be loaded. Will now try to install it from Bioconductor (requires working internet connection):\n");
-
-      # To please R CMD check
-      biocLite <- NULL; rm(list="biocLite");
-      source("http://www.bioconductor.org/biocLite.R");
-      biocLite(package);
-      # Assert that the package can be successfully loaded
-      res <- requireX(package, character.only=TRUE);
-      if (!res) {
-        throw("Package 'affxparser' could not be loaded. Please install it from Bioconductor, cf. http://www.bioconductor.org/");
-      }
-    } else {
-      warning("Package '", package, "' could not be loaded. Without it ", neededBy, " will not work. Please install it from Bioconductor, cf. http://www.bioconductor.org/");
-    }
-  }
-} # .requireBioCPackage()
-
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # affxparser related
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Instead of asking users to write affxparser::writeCdf() when
@@ -108,8 +74,6 @@ writeCdf.default <- function(...) {
 # 2013-08-03
 # o Added writeCdf.default() which loads namespace 'affxparser' and calls
 #   writeCdf() of affxparser, i.e. effectively affxparser::writeCdf().
-# o Added .requireAffxparser() which basically require("affxparser"),
-#   but will also try to install 'affxparser' if its missing.
 # 2009-02-22
 # o Removed code checking for package updates. Was never called.
 # 2009-02-21

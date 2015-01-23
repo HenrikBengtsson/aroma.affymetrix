7283d4d0161a553ad76d134257ed9a17de50392f
hb <hb@aroma-project.org>
2015-01-23 09:48:36 -0800

    BUG FIX: getMonocellCdf() would throw "Error in (...) : 3 arguments passed to '(' which requires 1".

---
+++ tests/AffymetrixCdfFile.R
@@ -0,0 +1,28 @@
+library("aroma.affymetrix")
+
+if (setupExampleData(aroma.affymetrix, dataset="FusionSDK_Test3", mustWork=FALSE)) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # CDF file
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  cdf <- AffymetrixCdfFile$byChipType("Test3")
+  print(cdf)
+
+  # Unit names, types, ...
+  names <- getUnitNames(cdf)
+  str(names)
+
+  units <- indexOf(cdf, names=names[42:40])
+  stopifnot(all.equal(units, 42:40))
+
+  types <- getUnitTypes(cdf)
+  print(table(types))
+
+  data <- readUnits(cdf, units=1:10)
+  str(data)
+
+  data <- readDataFrame(cdf, units=1:10)
+  str(data)
+
+  md5 <- getChecksumFile(cdf)
+  print(md5)
+} # if (... "AffymetrixDataTestFiles")

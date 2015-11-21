ab4e4c5543bfff4118079b54911b770813271059
hb <hb@aroma-project.org>
2015-11-21 15:17:47 -0800

    FUTURE: Help identifying some globals, cf. HenrikBengtsson/globals#10

--- R/AbstractProbeSequenceNormalization.R
+++ R/AbstractProbeSequenceNormalization.R
@@ -383,6 +383,9 @@ setMethodS3("process", "AbstractProbeSequenceNormalization", function(this, ...,
 
 
     res[[kk]] %<=% {
+      ## Help identifying some globals:
+      modelFit
+
       # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
       # Phase I: Fit probe-sequence effect for the current array
       # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

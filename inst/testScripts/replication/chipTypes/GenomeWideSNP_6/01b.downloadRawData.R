a0adf14d590628a9d24ca4082c7dce24f5e2f708
hb <hb@aroma-project.org>
2015-12-23 17:27:10 -0800

    Speeding your the complete test runs for GenomeWideSNP_6 by processing 6 instead of 14 samples.

--- inst/testScripts/replication/chipTypes/GenomeWideSNP_6/01b.downloadRawData.R
+++ inst/testScripts/replication/chipTypes/GenomeWideSNP_6/01b.downloadRawData.R
@@ -13,9 +13,9 @@ verbose && enter(verbose, "Downloading raw data");
 #
 # Overall design:
 #  21 replicates of HCC1143 (breast ductal carcinoma), 21 replicates
-#  of HCC1143BL (matched normal), 13 replicates of HCC1954 (breast 
-#  ductal carcinoma), 11 replicates of HCC1954BL (matched normal), 
-#  1 replicate of NCI-H2347 (lung adenocarcinoma), 1 replicate of 
+#  of HCC1143BL (matched normal), 13 replicates of HCC1954 (breast
+#  ductal carcinoma), 11 replicates of HCC1954BL (matched normal),
+#  1 replicate of NCI-H2347 (lung adenocarcinoma), 1 replicate of
 #  NCI-H2347BL (matched normal).
 #  http://www.broad.mit.edu/cancer/pub/solexa_copy_numbers/
 #
@@ -26,17 +26,17 @@ chipType <- "GenomeWideSNP_6";
 
 sampleNamesMap <- c(
   GSM337641="HCC1143_GLEYS_A02",
-  GSM337646="HCC1143_TRIBE_H11",
+#  GSM337646="HCC1143_TRIBE_H11",
   GSM337662="HCC1143BL_GLEYS_A01",
-  GSM337666="HCC1143BL_TRIBE_D02",
-  GSM337668="HCC1143BL_GHATS_H04",
-  GSM337674="HCC1143BL_TRIGS_G07",
+#  GSM337666="HCC1143BL_TRIBE_D02",
+#  GSM337668="HCC1143BL_GHATS_H04",
+#  GSM337674="HCC1143BL_TRIGS_G07",
   GSM337683="HCC1954_GLEYS_B02",
-  GSM337688="HCC1954_TRIBE_G12",
+#  GSM337688="HCC1954_TRIBE_G12",
   GSM337696="HCC1954BL_GLEYS_B01",
-  GSM337700="HCC1954BL_TRIBE_B01",
-  GSM337702="HCC1954BL_GHATS_G10",
-  GSM337703="HCC1954BL_TRIGS_G11",
+#  GSM337700="HCC1954BL_TRIBE_B01",
+#  GSM337702="HCC1954BL_GHATS_G10",
+#  GSM337703="HCC1954BL_TRIGS_G11",
   GSM337707="NCI-H2347",
   GSM337708="NCI-H2347BL"
 );

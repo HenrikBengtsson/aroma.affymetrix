8376b1108461f8f857c2b3c93e6f5db4e8255f15
hb <hb@aroma-project.org>
2016-01-01 15:28:21 -0800

    TESTS: Testing missing CDF

--- tests/AffymetrixCdfFile.R
+++ tests/AffymetrixCdfFile.R
@@ -4,6 +4,10 @@ library("aroma.affymetrix")
 cdf <- AffymetrixCdfFile()
 print(cdf)
 
+## Missing CDF
+cdf <- AffymetrixCdfFile(NA_character_, mustExist=FALSE)
+print(cdf)
+
 
 if (setupExampleData(aroma.affymetrix, dataset="FusionSDK_Test3", dirs="annotationData", mustWork=FALSE)) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

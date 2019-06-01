05d01d596ddd6d34c31997f71ce4862d103e3dff
Henrik Bengtsson <hb@aroma-project.org>
2019-05-31 19:41:00 -0700

    CLEANUP: Drop trailing semicolons

--- inst/testScripts/setup/99a.cleanupRootDirectories.R
+++ inst/testScripts/setup/99a.cleanupRootDirectories.R
@@ -1,11 +1,11 @@
-library("R.utils");
-if (interactive()) library("R.menu");
-verbose <- Arguments$getVerbose(-8, timestamp=TRUE);
+library("R.utils")
+if (interactive()) library("R.menu")
+verbose <- Arguments$getVerbose(-8, timestamp=TRUE)
 
 
-verbose && enter(verbose, "Removing all generated data");
+verbose && enter(verbose, "Removing all generated data")
 
-verbose && cat(verbose, "This will *not* remove annotation data or raw data sets.");
+verbose && cat(verbose, "This will *not* remove annotation data or raw data sets.")
 
 # Root directories to be removed, iff existing
 paths <- c(
@@ -25,28 +25,28 @@ paths <- c(
   "smoothCnData/",
   "reports/",
   "figures/"
-);
+)
 
 # Identify which exists
-keep <- sapply(paths, FUN=isDirectory);
-paths <- paths[keep];
+keep <- sapply(paths, FUN=isDirectory)
+paths <- paths[keep]
 
 if (length(paths) > 0) {
   if (interactive()) {
-    paths <- selectMenu(paths, selected=TRUE, title="Root directories to be removed");
-    verbose && cat(verbose, "Root directories to be removed:");
-    verbose && cat(verbose, paste(paths, collapse="\n"));
+    paths <- selectMenu(paths, selected=TRUE, title="Root directories to be removed")
+    verbose && cat(verbose, "Root directories to be removed:")
+    verbose && cat(verbose, paste(paths, collapse="\n"))
   }
   
   
   for (kk in seq_along(paths)) {
-    path <- paths[kk];
-    verbose && enter(verbose, sprintf("Root directory #%s ('%s') of %s", kk, path, length(paths)));
-    removeDirectory(path, recursive=TRUE);
-    verbose && exit(verbose);
+    path <- paths[kk]
+    verbose && enter(verbose, sprintf("Root directory #%s ('%s') of %s", kk, path, length(paths)))
+    removeDirectory(path, recursive=TRUE)
+    verbose && exit(verbose)
   } # for (kk ...)
 } else {
-  verbose && cat(verbose, "No root directories available. Skipping!");
+  verbose && cat(verbose, "No root directories available. Skipping!")
 }
 
-verbose && exit(verbose);
+verbose && exit(verbose)
